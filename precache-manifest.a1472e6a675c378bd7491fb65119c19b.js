2fd9b88d35445550977215ce7c6bb9007c71e077
PeterTheOne <petertheone@gmail.com>
2020-01-11 20:08:54 +0100

    Updates

---
+++ precache-manifest.a1472e6a675c378bd7491fb65119c19b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364663c2a495e63cf9f5768ce5d89464",
+    "url": "/newsR/index.html"
+  },
+  {
+    "revision": "74ad1cbd125fa9a512ed",
+    "url": "/newsR/static/css/main.4ff8bdb1.chunk.css"
+  },
+  {
+    "revision": "e9521c005b9d6da45540",
+    "url": "/newsR/static/js/2.5b3cdbb5.chunk.js"
+  },
+  {
+    "revision": "487720c13cc5bde46fede711bd00f4c6",
+    "url": "/newsR/static/js/2.5b3cdbb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74ad1cbd125fa9a512ed",
+    "url": "/newsR/static/js/main.a4ceae25.chunk.js"
+  },
+  {
+    "revision": "f21b9326b657fe200002",
+    "url": "/newsR/static/js/runtime-main.53d02f2f.js"
+  }
+]);
