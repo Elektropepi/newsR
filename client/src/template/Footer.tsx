070b0e51ff8ccd9b9900c88b74c421e9788ebbda
PeterTheOne <petertheone@gmail.com>
2020-01-24 16:39:41 +0100

    add version number to footer

--- client/src/template/Footer.tsx
+++ client/src/template/Footer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import packageJson from '../../package.json';
 
 export function Footer() {
   return (
     <div className="app-grid-footer">
-      <span className="app-name">newsR</span>
+      <span className="app-name">newsR {packageJson.version}</span>
       <a href="https://github.com/Elektropepi/newsR/" className="no-link github-link">
         <FontAwesomeIcon icon={['fab', 'github']} />&nbsp;&nbsp;Github
       </a>
